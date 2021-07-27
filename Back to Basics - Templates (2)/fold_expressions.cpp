c754c23b739bac0e0f319b8b4e55ba07a1287bc7
PatriotRossii <patriotrossii2019@mail.ru>
2021-07-27 18:04:50 +0000

    Add another example of function using fold expressions

--- Back to Basics - Templates (2)/fold_expressions.cpp
+++ Back to Basics - Templates (2)/fold_expressions.cpp
@@ -1,4 +1,13 @@
 #include <iostream>
+#include <string>
+
+auto Normalize(const std::string& t) { return t; }
+auto Normalize(const char* t) { return std::string{t}; }
+
+template<class T>
+auto Normalize(const T& t) {
+    return std::to_string(t);
+}
 
 template<typename T, typename... Ts>
 void print(const T& targ, const Ts&... args) {
@@ -9,6 +18,20 @@ void print(const T& targ, const Ts&... args) {
     (..., coutSpaceAndArg(args));
 }
 
+template<typename T, typename... Ts>
+auto buildCSVLine(const T& targ, const Ts&... args) {
+    auto ret{Normalize(targ)};
+    auto addColonAndNormalize = [&](const auto& arg) {
+        ret += ',';
+        ret += Normalize(arg);
+    };
+    (..., addColonAndNormalize(args));
+    return ret;
+}
+
 int main() {
     print("Hello", "C++", 20);
+
+    auto s = buildCSVLine("Hello", std::string{"C++"}, 20, "@CppCon");
+    std::cout << '\n' << s.c_str() << '\n';
 }
