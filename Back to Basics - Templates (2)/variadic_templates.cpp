8a8827234b02c05ae87d6f7cf4cdfe038dbc8814
PatriotRossii <patriotrossii2019@mail.ru>
2021-07-27 18:20:52 +0000

    Add another example of fold expression usage

--- Back to Basics - Templates (2)/variadic_templates.cpp
+++ Back to Basics - Templates (2)/variadic_templates.cpp
@@ -1,5 +1,7 @@
 #include <string>
 
+#include <utility>
+
 auto Normalize(const std::string& t) { return t; }
 auto Normalize(const char* t) { return std::string{t}; }
 
@@ -27,4 +29,45 @@ template<typename T, typename... Ts> auto StrCat(
     return ret;
 }
 
-int main() { }
+namespace details::array_single_compare {
+    template<typename T, size_t N, typename U, size_t... I>
+    constexpr bool Compare(const T (&a)[N], const U& b, std::index_sequence<I...>) {
+        return ((a[I] == b) && ... );
+    }
+}
+
+template<typename T, size_t N, typename U>
+constexpr bool Compare(const T (&a)[N], const U& b) {
+    return details::array_single_compare::Compare(a, b, std::make_index_sequence<N>{});
+}
+
+
+namespace details::array_compare {
+    template<typename T, size_t N, size_t... I>
+    constexpr bool Compare(const T (&a)[N], const T (&b)[N], std::index_sequence<I...>) {
+        return ((a[I] == b[I]) && ...);
+    }
+}
+
+template<typename T, size_t N>
+constexpr bool Compare(const T (&a)[N], const T (&b)[N]) {
+    return details::array_compare::Compare(a, b, std::make_index_sequence<N>{});
+}
+
+struct MACAddress {
+    unsigned char value[6];
+};
+
+int main() {
+    constexpr MACAddress macA{2, 2, 2, 2, 2, 2};
+    constexpr MACAddress macB{2, 2, 2, 2, 2, 4};
+    constexpr MACAddress macC{2, 2, 2, 2, 2, 2};
+
+    // Compare each element against value
+    static_assert(Compare(macA.value, 2));
+    static_assert(!Compare(macB.value, 2));
+
+    // Compare two equally sized arrays
+    static_assert(!Compare(macA.value, macB.value));
+    static_assert(Compare(macA.value, macC.value));
+}
